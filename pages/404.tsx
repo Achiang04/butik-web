c388e52c0babcb57bb7d1e267e7af4599c8d4192
jecky hindrawan <jecky@coconutlab.io>
2022-01-06 23:21:26 +0700

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,27 @@
+import Footer from 'components/Footer';
+import Header from 'components/Header';
+import Link from 'next/link';
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <>
+            <Header />
+
+            <div className="flex flex-col items-center pt-20">
+                <img className="w-1/2 h-160" src="/img/404.png" alt="" />
+                <Link href="/home">
+                    <div className="cursor-pointer py-2 px-8 bg-pink text-white rounded mb-10">
+                        <p>Back To Home</p>
+                    </div>
+                </Link>
+            </div>
+
+            <Footer />
+        </>
+    );
+};
+
+NotFound.defaultProps = {};
+
+export default React.memo(NotFound);
